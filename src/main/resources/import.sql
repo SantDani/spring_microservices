3f7ca8bfe401ceb69ddfbd4858e47e968847fb82
srodrigezSSD <srod>
2022-10-31 18:24:37 +0100

    insert mock products and get all

---
+++ src/main/resources/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO products (name, price, create_at) VALUES ('Panasonic', 800, NOW());
+INSERT INTO products (name, price, create_at) VALUES ('Sony', 500, NOW());
+INSERT INTO products (name, price, create_at) VALUES ('AMD', 800, NOW());
+INSERT INTO products (name, price, create_at) VALUES ('Intel', 9900, NOW());
+INSERT INTO products (name, price, create_at) VALUES ('NVIDIA', 3500, NOW());
+INSERT INTO products (name, price, create_at) VALUES ('Oracle', 800, NOW());
+INSERT INTO products (name, price, create_at) VALUES ('Heroku', 5500, NOW());
+INSERT INTO products (name, price, create_at) VALUES ('Xiaomi', 1000, NOW());
